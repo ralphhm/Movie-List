c558db4df61590d53b39167b7e8d11ea1530ed73
ralphhm <ralphhm+github@gmail.com>
2018-06-12 18:03:08 +0200

    Implement retry on failure logic

--- app/src/main/java/de/rhm/movielist/MovieListViewModel.kt
+++ app/src/main/java/de/rhm/movielist/MovieListViewModel.kt
@@ -3,12 +3,29 @@ package de.rhm.movielist
 import de.rhm.movielist.api.model.MovieListResult
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.subjects.PublishSubject
 
 class MovieListViewModel(movieListRepository: MovieListRepository) {
 
-    val uiState: Observable<out MovieListUiState> = movieListRepository.getMovies().toObservable().map { Result(it) as MovieListUiState}.onErrorReturn { Failure(it, {}) }.startWith(Loading).observeOn(AndroidSchedulers.mainThread())
+    private val uiAction = PublishSubject.create<FetchMovieListAction>()
+    val uiState: Observable<out MovieListUiState> = uiAction
+            //trigger fetch on ViewModel creation
+            .startWith(FetchMovieListAction)
+            //on every trigger prepare a new fetch
+            .switchMap {
+                movieListRepository.getMovies().toObservable()
+                        //map success case
+                        .map<MovieListUiState> { Result(it) }
+                        //emit loading state while fetching
+                        .startWith(Loading)
+                        //emit failure state if fetch failed providing an action that triggers another fetch
+                        .onErrorReturn { Failure(it, { uiAction.onNext(FetchMovieListAction) }) }
+            }
+            .observeOn(AndroidSchedulers.mainThread())
 
 }
+
+object FetchMovieListAction
 
 sealed class MovieListUiState
 class Result(val movies: List<MovieListResult>) : MovieListUiState()
