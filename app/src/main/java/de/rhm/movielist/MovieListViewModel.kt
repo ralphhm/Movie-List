a09276dd865d6c6484494cc84e0928f4f4042dc8
ralphhm <ralphhm+github@gmail.com>
2018-06-14 23:11:43 +0200

    Switch back to previous Result state if loading next page fails

--- app/src/main/java/de/rhm/movielist/MovieListViewModel.kt
+++ app/src/main/java/de/rhm/movielist/MovieListViewModel.kt
@@ -20,12 +20,14 @@ class MovieListViewModel(repository: MovieListRepository) : ViewModel() {
             .startWith(FetchMovieListAction())
             //on every trigger prepare a new fetch
             .compose(ActionToState({ action -> repository.getMovies(action.filterDate, action.page) }, { action -> uiAction.onNext(action) }))
+            .distinctUntilChanged()
             .scan { last: MovieListUiState, current: MovieListUiState ->
                 when {
                     //when Result is followed by Loading with the same date
-                    last is Result && current is Loading && last.filterDate == current.filterDate -> LoadingMore(last.movies)
+                    last is Result && current is Loading && last.filterDate == current.filterDate -> LoadingMore(last)
                     //when LoadingMore is followed by a successful Result, movie lists need to be aggregated
-                    last is LoadingMore && current is Result -> current.copy(last.movies + current.movies)
+                    last is LoadingMore && current is Result -> current.copy(last.lastResult.movies + current.movies)
+                    last is LoadingMore && current is Failure -> last.lastResult
                     else -> current
                 }
             }
@@ -41,8 +43,8 @@ data class FetchMovieListAction(val filterDate: Date = Date(), val page: Int = 1
 sealed class MovieListUiState
 data class Result(val movies: MovieList, val filterDate: Date, val filterAction: (Date) -> Unit, val loadMoreAction: () -> Unit) : MovieListUiState()
 class Failure(val cause: Throwable, val retryAction: () -> Unit) : MovieListUiState()
-class Loading(val filterDate: Date) : MovieListUiState()
-class LoadingMore(val movies: MovieList): MovieListUiState()
+data class Loading(val filterDate: Date) : MovieListUiState()
+class LoadingMore(val lastResult: Result): MovieListUiState()
 
 class ActionToState(
         private inline val call: (FetchMovieListAction) -> Single<MovieList>,
@@ -56,7 +58,7 @@ class ActionToState(
                 //emit loading state while fetching
                 .toObservable().startWith(Loading(action.filterDate))
                 //emit failure state if fetch failed providing an action that triggers another fetch
-                .onErrorReturn { Failure(it, { nextAction.invoke(action) }) }
+                .onErrorReturn { Failure(it) { nextAction.invoke(action) } }
     }
 
 }
